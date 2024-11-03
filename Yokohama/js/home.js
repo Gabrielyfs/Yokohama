f9765f38b1cc505bba28feb53fa398c78a813ace
Gabriely Félix <felixdesouzagabriely@gmail.com>
2024-11-03 20:56:10 -0300

    Add files via upload

--- Yokohama/js/home.js
+++ Yokohama/js/home.js
@@ -13,6 +13,25 @@ function proximaImg() {
 
     document.getElementById('radio' + count).checked = true;
 
+    //reiniciar animação
+    resetAnimation();
+
+}
+
+function resetAnimation() {
+    let animatedElements = document.querySelectorAll('.slider-titulo, .slider-sub');
+
+     // Remova e adicione a classe de animação para reiniciar
+    animatedElements.forEach(element => {
+        // Remova qualquer animação em andamento
+        element.style.animation = 'none';
+        
+        // Forçar reflow para garantir que a animação seja reiniciada
+        element.offsetHeight;
+        
+        // Adicione a animação novamente
+        element.style.animation = 'sliderReveal 1.5s ease forwards';
+    });
 }
 
 function pagSobre() {
