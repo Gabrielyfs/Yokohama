34af40937d9a4476e996fc72e7ff9aaa2ad2df94
Gabriely Félix <felixdesouzagabriely@gmail.com>
2024-11-04 23:05:12 -0300

    Add files via upload

--- Yokohama/js/menu.js
+++ Yokohama/js/menu.js
@@ -1,5 +1,11 @@
 const logoL = document.querySelector('.logoL')
+const logos = document.querySelector('.logos')
 
 logoL.addEventListener('click', () => {
     window.location.href = '../html/home.html'
-})
+});
+logos.addEventListener('click', () => {
+    window.location.href = '../html/home.html'
+});
+
+
