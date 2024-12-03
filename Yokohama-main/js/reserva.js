1b1b89f502c03e55b50dbf78e6f1c981f8249f8f
Gabriely Félix <felixdesouzagabriely@gmail.com>
2024-12-03 15:47:35 -0300

    Delete Yokohama-main directory

--- Yokohama-main/js/reserva.js
+++
@@ -1,9 +0,0 @@
-const logoL = document.querySelector('.logoL')
-const logoF = document.querySelector('.logoF')
-
-logoL.addEventListener('click', () => {
-    window.location.href = '../html/home.html'
-});
-logoF.addEventListener('click', () => {
-    window.location.href = '../html/home.html'
-});
